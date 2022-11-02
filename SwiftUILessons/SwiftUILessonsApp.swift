575efbc1e170d03e24ca4b284eb422f69ea3136e
Alessandro Pace <alessandropacec@gmail.com>
2022-11-02 16:21:04 +0100

    Project Start

---
+++ SwiftUILessons/SwiftUILessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILessonsApp.swift
+//  SwiftUILessons
+//
+//  Created by Alessandro on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
